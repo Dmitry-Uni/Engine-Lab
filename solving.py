330ffe3065ce1d638ea091d9f81d31925b1d7d31
Dmitry-Uni <dvol0706@uni.sydney.edu.au>
2025-11-08 10:54:33 +1100

    Adding stroke plots

--- solving.py
+++ solving.py
@@ -1,6 +1,14 @@
 from preprocessing import get_data
 
 # =============================================================================
-    #Air Consumption
+    #Stroke plots
 # =============================================================================
+
+unit, RPM = get_data(("DTS2 Engine Torque & Speed", "Speed"), ("DTS2 Engine Torque & Speed", "Speed"))
+
+minRPM = min(RPM[0][2:-2])
+minRPMid = list(RPM[0]).index(minRPM)
+
+maxRPM = max(RPM[0])
+maxRPMid = list(RPM[0]).index(maxRPM)
 
